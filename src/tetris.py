b1c4177193533f0bd5d4d1ca7da1ee63cbdade01
Maija <maija.saisa@helsinki.fi>
2022-11-28 19:55:22 +0200

    Lisätty palikan törmääminen seiniin

--- src/tetris.py
+++ src/tetris.py
@@ -15,9 +15,38 @@ class Tetris:
 
     def move_down(self):
         self.block.y += 1
+        if self.collision():
+            self.block.y -= 1
+            self.freeze()
 
     def move_sideways(self, direction):
+        current_x = self.block.x
         self.block.x += direction
-
+        if self.collision():
+            self.block.x = current_x
+        
     def rotate(self):
         self.block.rotate()
+        
+    def collision(self):
+        collide = False
+        for Y in range(4):
+            for X in range(1, 5):
+                square = Y*4 + X
+                if square in self.block.figure():
+                    if self.block.y + Y > self.height -1:
+                        collide = True
+                    elif self.block.x + X > self.width:
+                        collide = True
+                    elif self.block.x + X < 1:
+                        collide = True
+        return collide
+    
+    def freeze(self):
+        for Y in range(4):
+            for X in range(1, 5):
+                square = Y*4 + X
+                if square in self.block.figure():
+                    pass
+    
+    
