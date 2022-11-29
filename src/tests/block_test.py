523b6fa2d09e4b06a3c688cd9b33c848f4d4c978
Maija <maija.saisa@helsinki.fi>
2022-11-29 22:53:41 +0200

    Formatointikorjauksia

--- src/tests/block_test.py
+++ src/tests/block_test.py
@@ -12,7 +12,7 @@ class TestBlock(unittest.TestCase):
     def test_block_initialization_location_is_correct(self):
         self.assertEqual(self.block.x, 4)
         self.assertEqual(self.block.y, 0)
-        
+
     def test_block_initialization_rotation_is_correct(self):
         self.assertEqual(self.block.rotation, 0)
 
@@ -20,13 +20,13 @@ class TestBlock(unittest.TestCase):
         self.block.shape = 1
         self.block.rotate()
         self.assertEqual(self.block.rotation, 1)
-        
+
     def test_block_rotation_works_with_large_i_and_returns_to_zero(self):
         self.block.shape = 1
         self.block.rotate()
         self.block.rotate()
         self.assertEqual(self.block.rotation, 0)
-        
+
     def test_method_figure_returns_correct_figure(self):
         self.block.shape = 3
         self.assertEqual(self.block.figure(), [5, 6, 10, 11])
--- src/tests/tetris_test.py
+++ src/tests/tetris_test.py
@@ -12,16 +12,16 @@ class TestTetris(unittest.TestCase):
     def test_tetris_initialization_size_is_correct(self):
         self.assertEqual(self.tetris.height, 20)
         self.assertEqual(self.tetris.width, 10)
-        
+
     def test_tetris_initialization_state_is_play(self):
         self.assertEqual(self.tetris.state, "play")
-        
+
     def test_tetris_initialization_score_is_zero(self):
         self.assertEqual(self.tetris.score, 0)
-        
+
     def test_tetris_initialization_block_is_none(self):
         self.assertEqual(self.tetris.block, None)
- 
+
     def test_method_new_block_creates_block(self):
         self.tetris.new_block()
         self.assertNotEqual(self.tetris.block, None)
@@ -31,19 +31,19 @@ class TestTetris(unittest.TestCase):
         y = self.tetris.block.y
         self.tetris.move_down()
         self.assertEqual(self.tetris.block.y, y+1)
-        
+
     def test_method_move_sideways_works_when_moving_left(self):
         self.tetris.new_block()
         x = self.tetris.block.x
         self.tetris.move_sideways(-1)
         self.assertEqual(self.tetris.block.x, x-1)
-        
+
     def test_method_move_sideways_works_when_moving_right(self):
         self.tetris.new_block()
         x = self.tetris.block.x
         self.tetris.move_sideways(1)
         self.assertEqual(self.tetris.block.x, x+1)
-        
+
     def test_method_rotate_works(self):
         self.tetris.new_block()
         self.tetris.rotate()
