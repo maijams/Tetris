dc308ca3279afd42e1fbb51d349e85d296c0b85d
Maija <maija.saisa@helsinki.fi>
2022-12-13 16:15:49 +0200

    Lisätty docstring luokkiin block & tetris

--- src/block.py
+++ src/block.py
@@ -12,7 +12,13 @@ LIGHTBLUE = (45, 189, 184)
 
 
 class Block:
-    # Possible positions in 4x4 grid
+    '''Class that handles the events of a falling block.
+    
+    Attributes:
+        location_x: Horizontal coordinate of the block in Pygame grid.
+        location_y: Vertical coordinate of the block in Pygame grid.
+    '''
+    
     blocks = [
         [[1, 2, 5, 6]],
         [[1, 5, 9, 13], [4, 5, 6, 7]],
@@ -25,24 +31,51 @@ class Block:
 
     colors = [BLUE, PINK, GREEN, PURPLE, YELLOW, RED, ORANGE, LIGHTBLUE]
 
+
     def __init__(self, location_x, location_y):
+        '''Class constructor that creates a new falling block by randomly choosing the shape & color.
+        
+        Args:
+            pos_x: Horizontal coordinate of the block in Pygame grid.
+            pos_y: Vertical coordinate of the block in Pygame grid.
+        '''
+        
         self.pos_x = location_x
         self.pos_y = location_y
         self.shape = random.randint(0, len(self.blocks)-1)
         self.rotation = 0
         self.color = self.colors[random.randint(0, len(self.colors)-1)]
 
+
     def rotate(self):
+        '''Rotates the block by changing the values in 4x4 area.
+        
+        If rotation value is already at the last possible rotation, rotation returns to zero.
+        Else rotation is increased by one.
+        '''
+        
         if self.rotation == len(self.blocks[self.shape])-1:
             self.rotation = 0
         else:
             self.rotation += 1
 
+
     def reverse_rotate(self):
+        '''Reverses the block rotation if the block collides with walls or other blocks.
+        
+        If rotation value is zero, rotation value is changed to last possible rotation.
+        Else rotation is decreased by one.
+        '''
+        
         if self.rotation == 0:
             self.rotation = len(self.blocks[self.shape])-1
         else:
             self.rotation -= 1
 
+
     def figure(self):
+        '''Returns:
+            The current state of the block, i.e. the block shape & rotation combination.
+        '''
+        
         return self.blocks[self.shape][self.rotation]
--- src/tetris.py
+++ src/tetris.py
@@ -2,7 +2,22 @@ from block import Block
 
 
 class Tetris:
+    '''Class that handles the game state & events.
+    
+    Attributes:
+        height: The height of the game grid.
+        width: The width of the game grid.
+    '''
+    
+    
     def __init__(self, height, width):
+        '''Class constructor that creates a new Tetris game.
+        
+        Args:
+            height: The height of the game grid.
+            width: The width of the game grid.
+        '''
+        
         self.height = height
         self.width = width
         self.state = "play"
@@ -10,27 +25,58 @@ class Tetris:
         self.field = [[0]*width for _ in range(height)]
         self.points = 0
 
+
     def new_block(self):
+        '''Creates a new falling block at the top of the game grid.'''
+        
         self.block = Block(3, 0)
 
+
     def move_down(self):
+        '''Moves the falling block one step downwards. 
+        
+        If block collides with other objects, the step is reversed and the block freezes.
+        '''
+        
         self.block.pos_y += 1
         if self.collision():
             self.block.pos_y -= 1
             self.freeze()
 
+
     def move_sideways(self, direction):
+        '''Moves the falling block in the given horizontal direction.
+        
+        If block collides with other objects, the step is reversed.
+        
+        Args:
+            direction: Direction that block should be moved to. -1 = left, 1 = right.
+        '''
+        
         current_x = self.block.pos_x
         self.block.pos_x += direction
         if self.collision():
             self.block.pos_x = current_x
 
+
     def rotate(self):
+        '''Rotates the falling block.
+        
+        If block collides with other objects, the step is reversed.
+        '''
+        
         self.block.rotate()
         if self.collision():
             self.block.reverse_rotate()
 
+
     def collision(self):
+        '''Checks whether the falling block collides with walls, floor or other blocks.
+        
+        Returns:
+            True, if collision happens. False, if no collision.
+        '''
+        
         collide = False
         for field_y in range(4):
             for field_x in range(4):
@@ -46,7 +92,15 @@ class Tetris:
                         collide = True
         return collide
 
+
     def freeze(self):
+        '''Updates the game field according to frozen block coordinates.
+        
+        Remove full rows & create a new block.
+        
+        If collision happens with the new block, game state is changed to "end".
+        '''
+        
         for field_y in range(4):
             for field_x in range(4):
                 square = field_y*4 + field_x
@@ -58,7 +112,10 @@ class Tetris:
         if self.collision():
             self.state = "end"
 
+
     def remove_rows(self):
+        '''Remove full rows if they exist. Update points accordingly.'''
+        
         rows = 0
         for field_y in range(1, self.height):
             if self.field[field_y].count(0) == 0:
@@ -68,8 +125,17 @@ class Tetris:
                         self.field[i][field_x] = self.field[i-1][field_x]
         self.points += rows**2 * self.width
 
+
     def get_points(self):
+        '''Returns:
+            The amount of current points.
+        '''
+        
         return self.points
 
+
     def get_state(self):
+        '''Returns:
+            The current state of the game.
+        '''
         return self.state
